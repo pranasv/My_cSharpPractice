b378fc71c30473dfb8265adcba4380817c1d9a19
LAPTOP-73BPNIN4\pranas <pranas.vaicius@gmail.com>
2018-10-26 22:05:52 +0300

    first commit

---
+++ tst1/Item.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tst1
+{
+    class Item
+    {
+        public static List<Item> id = new List<Item>();
+        private int ID { get; set; }
+        private List<int> FatherID { get;  set; } = new List<int>();
+        private List<int> ChildID { get;  set; } = new List<int>();
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Item()
+        {
+            this.ID = ID_Counter.value;
+            ID_Counter.value++;
+            id.Add(this);
+        }
+        public void AddSubItem(int subitemID = 0)
+        {
+            if (subitemID == 0)
+            {
+                var newitem = new Item();
+                ChildID.Add(newitem.ID);
+                newitem.FatherID.Add(this.ID);
+            }
+            else
+            {
+                ChildID.Add(subitemID);
+                Item.id[subitemID].FatherID.Add(this.ID);
+            }
+        }
+        public override string ToString()
+        {
+            string WhereUsed = "";
+            if (FatherID != null)
+            {
+                foreach (int item in FatherID)
+                {
+                    WhereUsed += item + "; ";
+                }
+            }
+            string Contains = "";
+            if (ChildID != null)
+            {
+                foreach (int item in ChildID)
+                {
+                    Contains += item + "; ";
+                }
+            }
+            return "ID: " + ID + "   Name: " + Name + "   Description: " + Description + "   Used in Items(ID): " + WhereUsed + "   Contains subitems(ID): " + Contains;
+        }
+
+    }
+}
---
+++ tst1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tst1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var item = new Item();
+            new Item();
+            item.AddSubItem();
+            item.AddSubItem();
+            item.AddSubItem();            
+            Console.WriteLine(item);
+            Console.WriteLine(Item.id[1]);
+            Item.id[2].AddSubItem(1);            
+            Console.WriteLine(Item.id[2]);
+            Console.ReadKey();
+        }
+    }
+}
